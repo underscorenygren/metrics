499a3774ee884954b5caa847293fab9dcad14f42
Erik Nygren <sten.erik.nygren@gmail.com>
2019-12-16 14:49:45 -0500

    added install of binary command, some running fixes

--- cmd/partaj-tail/main.go
+++ cmd/partaj-tail/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/underscorenygren/partaj/internal/logging"
 	"github.com/underscorenygren/partaj/internal/timeutil"
@@ -20,6 +21,11 @@ import (
 	"time"
 )
 
+var errMissingLogGroup = "ResourceNotFoundException"
+
+//Used with compile flags
+var version = "0.0.0"
+
 //Stream one log stream
 type Stream struct {
 	name  string
@@ -35,20 +41,25 @@ type Streamer struct {
 	logGroupName string
 }
 
+//creates the top level streamer that holds many streamers to many log groups+streams
 func newStreamer(logGroupName string) (*Streamer, error) {
 	if logGroupName == "" {
 		return nil, fmt.Errorf("logGroupName must be set")
 	}
 
-	return &Streamer{
+	streamer := &Streamer{
 		logGroupName: logGroupName,
 		client:       cloudwatch.NewClient(false),
 		logStreams:   map[string]*Stream{},
 		logger:       logging.Logger(),
-	}, nil
+	}
+
+	streamer.logger.Debug("created Streamer", zap.String("logGroupName", logGroupName))
+
+	return streamer, nil
 }
 
-//newStream creates a new stream
+//newStream creates a new source/sink for a log group name and stream
 func newStream(logGroupName, logStreamName string) (*Stream, error) {
 	cloudwatchSource, err := cloudwatch.NewSource(cloudwatch.SourceConfig{
 		LogGroupName:  logGroupName,
@@ -90,6 +101,7 @@ func newStream(logGroupName, logStreamName string) (*Stream, error) {
 	}, nil
 }
 
+//runs one stream
 func (s *Stream) run() {
 	logger := logging.Logger()
 	err := s.stage.Flow()
@@ -103,6 +115,7 @@ func (s *Stream) run() {
 	}
 }
 
+//calls the cloudwatchlogs list command once
 func (streamer *Streamer) list() ([]string, error) {
 	res := []string{}
 
@@ -126,40 +139,84 @@ func (streamer *Streamer) list() ([]string, error) {
 	return res, nil
 }
 
-//Run streams forever
-func (streamer *Streamer) Run() {
-	//Checks for new streams every second
-	for range time.Tick(1 * time.Second) {
-		streamer.logger.Debug("listing streams")
-		streams, err := streamer.list()
-		if err != nil {
-			streamer.logger.Error("couldn't list streams", zap.Error(err))
-		} else {
-			for _, streamName := range streams {
-				if _, ok := streamer.logStreams[streamName]; !ok {
-					streamer.logger.Debug("creating stream", zap.String("streamName", streamName))
-					s, err := newStream(streamer.logGroupName, streamName)
-					if err != nil {
-						streamer.logger.Error("couldn't create stream", zap.Error(err))
-					} else {
-						streamer.logStreams[streamName] = s
-						go s.run()
-					}
+//isFatal true iff cloudwatch error isn't retryable
+func isFatal(err error) bool {
+	if awsErr, ok := err.(awserr.Error); ok {
+		return awsErr.Code() == errMissingLogGroup
+	}
+	return false
+}
+
+//lists streams once
+func (streamer *Streamer) doList() error {
+	streamer.logger.Debug("listing streams")
+	streams, err := streamer.list()
+	if isFatal(err) {
+		return err
+	} else if err != nil {
+		streamer.logger.Error("couldn't list streams", zap.Error(err))
+	} else {
+		for _, streamName := range streams {
+			if _, ok := streamer.logStreams[streamName]; !ok {
+				streamer.logger.Debug("creating stream", zap.String("streamName", streamName))
+				s, err := newStream(streamer.logGroupName, streamName)
+				if err != nil {
+					streamer.logger.Error("couldn't create stream", zap.Error(err))
 				} else {
-					streamer.logger.Debug("stream pre-existing", zap.String("streamName", streamName))
+					streamer.logStreams[streamName] = s
+					go s.run()
 				}
+			} else {
+				streamer.logger.Debug("stream pre-existing", zap.String("streamName", streamName))
 			}
 		}
 	}
+
+	return nil
 }
 
+//Run streams forever
+func (streamer *Streamer) Run() error {
+	//Checks for new streams every second
+	err := streamer.doList()
+	if err != nil {
+		return err
+	}
+	for range time.Tick(3 * time.Second) {
+		err = streamer.doList()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//command entry point
 func main() {
 	var logGroupName string
+	var debug bool
+	var versionFlag bool
+
 	flag.StringVar(&logGroupName, "name", "", "the  name of the log group to tail")
+	flag.BoolVar(&debug, "debug", false, "sets logging to debug level")
+	flag.BoolVar(&versionFlag, "version", false, "prints version and exits")
+
 	flag.Parse()
+
+	if versionFlag {
+		fmt.Printf("%s\n", version)
+		return
+	}
+
+	if debug {
+		logging.SetLevel(zap.DebugLevel)
+		logging.Logger().Debug("setting to debug level")
+	}
+
 	streamer, err := newStreamer(logGroupName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	streamer.Run()
+	log.Fatal(streamer.Run())
 }
